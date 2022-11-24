9428bac04bf3973665d6307c31e20b81e7b44a48
Tyler Brown <tyler.brown@telus.com>
2022-11-23 17:42:39 -0700

    finalizing fetcher with stretch included

--- fetcher.js
+++ fetcher.js
@@ -1,19 +1,44 @@
 const fs = require('fs');
 const request = require('request');
-let args  = process.argv.slice(2);
+let args = process.argv.slice(2);
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 request(args[0], (error, response, body) => {
 
-  let bytes = body.length;
+  const fileWriter = function() {
+    fs.writeFile(args[1], body, error => {
+      let bytes = body.length;
 
-  fs.writeFile(args[1], body, error =>{
+      if (error) {
+        console.error(error);
+      }
+      console.log(`Downloaded and saved ${bytes} bytes to ${args[1]}`);
+      process.exit();
+    });
+  };
 
-    if(error){
-      console.error(error);
-    }
-    console.log(`Downloaded and saved ${bytes} bytes to ${args[1]}`)
-  })
-})
+  if (error) {
+    return console.error(error, response);
+  }
+  if (fs.existsSync(args[1])) {
+
+    rl.question('File Already exists, Press "y" then "enter" to overwrite file. Press "enter" to cancel ', (answer) => {
+      if (answer === "y") {
+        fileWriter();
+
+      } else process.exit();
+    });
+  } else {
+    fileWriter();
+  }
+});
+
+
 
 //args[0] is address to pull from.
 //args[1] is path where files will be stored.
